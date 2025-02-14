c81e66ec02f59671842a0884c43c51f8adccc29b
Emmanuel Serna Yanes <emmanuel240158@gmail.com>
2025-02-14 16:12:37 -0500

    fix(resolve conflict menu): change theme xml - change in the theme file and fix when the the user press one medicine the menu appears and resolve conflict when the user push back.

--- app/src/main/java/com/emman/android/medialarm/homeModule/HomeActivity.kt
+++ app/src/main/java/com/emman/android/medialarm/homeModule/HomeActivity.kt
@@ -1,10 +1,10 @@
 package com.emman.android.medialarm.homeModule
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.emman.android.medialarm.R
 import com.emman.android.medialarm.databinding.ActivityHomeBinding
@@ -16,15 +16,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityHomeBinding.inflate(layoutInflater)
-
         setContentView(_binding.root)
 
         val bottomNavigationView = _binding.bottomNavigationView
+        val floatingActionButton = _binding.fab
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
         val navController = navHostFragment.navController
-
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_treatment,
@@ -33,10 +31,25 @@ class HomeActivity : AppCompatActivity() {
                 R.id.navigation_user
             )
         )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.detailFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                    floatingActionButton.visibility = View.GONE
+                }
+
+                else -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                    floatingActionButton.visibility = View.VISIBLE
+                }
+            }
+        }
+
 
     }
 }
