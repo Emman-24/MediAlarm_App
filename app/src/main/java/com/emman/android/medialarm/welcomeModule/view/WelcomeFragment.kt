f634059b0aee7846b0758a8b51d25d6306c262a9
Emmanuel Serna Yanes <emmanuel240158@gmail.com>
2025-03-04 16:08:38 -0500

    feat : add Hilt integration for WelcomeFragment and WelcomeViewModel -Add @AndroidEntryPoint to fragment. -Implement @HiltViewModel with constructor injection. -Use viewModels delegate for lifecycle-aware VM access

--- app/src/main/java/com/emman/android/medialarm/welcomeModule/view/WelcomeFragment.kt
+++ app/src/main/java/com/emman/android/medialarm/welcomeModule/view/WelcomeFragment.kt
@@ -5,37 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.emman.android.medialarm.R
 import com.emman.android.medialarm.databinding.FragmentWelcomeBinding
 import com.emman.android.medialarm.welcomeModule.viewModel.WelcomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class WelcomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentWelcomeBinding
+    private lateinit var _binding: FragmentWelcomeBinding
+    private val _viewModel: WelcomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
-        val viewModel = ViewModelProvider(this)[WelcomeViewModel::class.java]
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
 
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = _viewModel
+        }
 
-
-        viewModel.navigateToPrivacyFragment.observe(viewLifecycleOwner) { navigate ->
+        _viewModel.navigateToPrivacyFragment.observe(viewLifecycleOwner) { navigate ->
             if (navigate) {
                 findNavController().navigate(R.id.welcomeFragment_to_privacyFragment)
-                viewModel.onNavigateToPrivacyFragmentComplete()
+                _viewModel.onNavigateToPrivacyFragmentComplete()
             }
         }
 
 
-        return binding.root
     }
 
 
