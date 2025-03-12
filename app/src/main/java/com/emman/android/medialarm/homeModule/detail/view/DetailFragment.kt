d0609b252a41141237b2a90500368ddad45c0632
Emmanuel Serna Yanes <emmanuel240158@gmail.com>
2025-03-12 16:28:23 -0500

    perf : improvements detail -update with dagger hilt.

--- app/src/main/java/com/emman/android/medialarm/homeModule/detail/view/DetailFragment.kt
+++ app/src/main/java/com/emman/android/medialarm/homeModule/detail/view/DetailFragment.kt
@@ -6,42 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.emman.android.medialarm.databinding.FragmentDetailBinding
 import com.emman.android.medialarm.homeModule.detail.viewmodel.DetailViewModel
-import com.emman.android.medialarm.homeModule.detail.viewmodel.DetailViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     private val args: DetailFragmentArgs by navArgs()
 
     private lateinit var _binding: FragmentDetailBinding
+    private val _viewModel: DetailViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
-
-        val medicineId = args.medicineId
-
-        val viewModel =
-            ViewModelProvider(this, DetailViewModelFactory(medicineId))[DetailViewModel::class.java]
-
-        _binding.viewModel = viewModel
-
-        _binding.lifecycleOwner = viewLifecycleOwner
-
         return _binding.root
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding.apply {
+            viewModel = _viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        _viewModel.getMedicineById(args.medicineId)
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             findNavController().navigate(DetailFragmentDirections.actionDetailFragmentToNavigationList())
@@ -50,6 +46,7 @@ class DetailFragment : Fragment() {
         _binding.topAppBar.setOnClickListener {
             findNavController().navigate(DetailFragmentDirections.actionDetailFragmentToNavigationList())
         }
+
     }
 
 
--- app/src/main/java/com/emman/android/medialarm/homeModule/detail/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/emman/android/medialarm/homeModule/detail/viewmodel/DetailViewModel.kt
@@ -2,42 +2,22 @@ package com.emman.android.medialarm.homeModule.detail.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.map
-import com.emman.android.medialarm.data.local.Data
 import com.emman.android.medialarm.data.local.Medicine
+import com.emman.android.medialarm.data.repository.MedicineRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class DetailViewModel(
-    private val medicineId: Long
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val medicineRepository: MedicineRepository
 ) : ViewModel() {
-
-    /*
-    Search for medicine in database with medicineId and assign it to medicine
-     */
-
-    val data = Data().listMedicines
 
     private val _medicine = MutableLiveData<Medicine>()
     var medicine: MutableLiveData<Medicine> = _medicine
 
-    private val _medicineName = MutableLiveData<String>()
-    var medicineName: MutableLiveData<String> = _medicineName
-
-    init {
-        findMedicineById(medicineId)
+    fun getMedicineById(medicineId: Long) {
+        _medicine.value = medicineRepository.getMedicineById(medicineId)
     }
-
-    private fun findMedicineById(id: Long) {
-        data.map { list ->
-            list.find { it.id == id }
-        }.observeForever { foundMedicine ->
-            foundMedicine?.let {
-                _medicine.value = it
-                _medicineName.value = it.name
-            }
-        }
-    }
-
-
 
 
 }
