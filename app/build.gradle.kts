be4447aac6cc788679e268eb4f8803b6dbfb1f40
Emmanuel Serna Yanes <emmanuel240158@gmail.com>
2025-04-22 22:38:58 -0500

    feat(remote): add Room,Gson,Retrofit and OkHttp in the build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,11 +69,35 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
+    /**
+     * Room
+     */
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    /**
+     * Gson
+     */
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    /**
+     * Retrofit
+     */
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    /**
+     * OkHttp
+     */
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
 
-kapt{
+kapt {
     correctErrorTypes = true
 }
